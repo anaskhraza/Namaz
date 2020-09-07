55b1785e3b1e037ca45c49c2e3af89d6bda540db
Anas Khurshid <anaskhurshid1991@gmail.com>
2020-09-07 10:51:08 +0300

    Initial Commit

---
+++ Quran and Namaz/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Quran and Namaz
+//
+//  Created by Anas khurshid on 9/7/20.
+//  Copyright © 2020 Anas khurshid. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Quran and NamazTests/Quran_and_NamazTests.swift
@@ -0,0 +1,34 @@
+//
+//  Quran_and_NamazTests.swift
+//  Quran and NamazTests
+//
+//  Created by Anas khurshid on 9/7/20.
+//  Copyright © 2020 Anas khurshid. All rights reserved.
+//
+
+import XCTest
+@testable import Quran_and_Namaz
+
+class Quran_and_NamazTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
