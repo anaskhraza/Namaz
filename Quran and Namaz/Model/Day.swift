a3ac32417452c5064694c2f3960e3e8560ed5847
Anas Khurshid <anaskhurshid1991@gmail.com>
2021-01-20 23:05:56 +0300

    changes

---
+++ Quran and Namaz/Model/Day.swift
@@ -0,0 +1,20 @@
+//
+//  Day.swift
+//  Quran and Namaz
+//
+//  Created by Anas khurshid on 20/01/2021.
+//  Copyright © 2021 Anas khurshid. All rights reserved.
+//
+
+import Foundation
+
+struct Day {
+  // 1
+  let date: Date
+  // 2
+  let number: String
+  // 3
+  let isSelected: Bool
+  // 4
+  let isWithinDisplayedMonth: Bool
+}
---
+++ Quran and Namaz/Model/MonthMetadata.swift
@@ -0,0 +1,15 @@
+//
+//  MonthMetadata.swift
+//  Quran and Namaz
+//
+//  Created by Anas khurshid on 20/01/2021.
+//  Copyright © 2021 Anas khurshid. All rights reserved.
+//
+
+import Foundation
+
+struct MonthMetadata {
+  let numberOfDays: Int
+  let firstDay: Date
+  let firstDayWeekday: Int
+}
