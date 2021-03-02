388a08dfe8495c318399abbba3c64d3607f5a2ff
Anas Khurshid <anaskhurshid1991@gmail.com>
2021-03-02 23:03:40 +0300

    changes

---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Quran and Namaz' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Quran and Namaz
+  pod 'JTAppleCalendar', '~> 7.1'
+  target 'Quran and NamazTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Quran and NamazUITests' do
+    # Pods for testing
+  end
+
+end
+
+target 'WidgetExtensionExtension' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WidgetExtensionExtension
+
+end
---
+++ Quran and Namaz/CalendarPicker/CalendarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarViewController.swift
+//  Quran and Namaz
+//
+//  Created by Anas khurshid on 02/03/2021.
+//  Copyright © 2021 Anas khurshid. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
