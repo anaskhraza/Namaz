274c4bcb5e3a48ddb928c8bd808ddbeac4321515
Anas Khurshid <anaskhurshid1991@gmail.com>
2021-03-16 14:09:13 +0300

    changes

---
+++ Quran and Namaz/Prayer/PrayerCalculation.swift
@@ -0,0 +1,9 @@
+//
+//  PrayerCalculation.swift
+//  Quran and Namaz
+//
+//  Created by Anas khurshid on 16/03/2021.
+//  Copyright © 2021 Anas khurshid. All rights reserved.
+//
+
+import Foundation
