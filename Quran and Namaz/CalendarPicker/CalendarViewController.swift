dda0c687d13a9e79e2d8224e8e9fb430976fa359
Muhammad Anas <mkhurshid@psa.gov.qa>
2021-07-05 13:35:09 +0300

    changes for calendarViewController

--- Quran and Namaz/CalendarPicker/CalendarViewController.swift
+++ Quran and Namaz/CalendarPicker/CalendarViewController.swift
@@ -10,38 +10,78 @@ import UIKit
 import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
-
+    
     @IBOutlet var calendarView: JTAppleCalendarView!
     @IBOutlet var islamicDate: UILabel!
+    @IBOutlet var year: UILabel!
+    @IBOutlet var month: UILabel!
+    
+    
+    let outsideMonthColor = UIColor(colorWithHexValue: 0x584a66)
+    let monthColor = UIColor.white
+    let selectedMonthColor = UIColor(colorWithHexValue: 0x3a294b)
+    let currentDateSelectedViewColor = UIColor(colorWithHexValue: 0x4e3f5d)
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCalendarView()
+        
+    }
+    func setupCalendarView() {
         calendarView.scrollDirection = .vertical
         calendarView.scrollingMode   = .stopAtEachCalendarFrame
         calendarView.showsHorizontalScrollIndicator = false
-    }
-    
-    func configureCell(view: JTAppleCell?, cellState: CellState) {
-        guard let cell = view as? DateCell  else { return }
-        cell.dateLabel.text = cellState.text
-        handleCellTextColor(cell: cell, cellState: cellState)
-    }
-    
-    func handleCellTextColor(cell: DateCell, cellState: CellState) {
-        if cellState.dateBelongsTo == .thisMonth {
-            cell.dateLabel.textColor = UIColor.black
-        } else {
-            cell.dateLabel.textColor = UIColor.gray
+        
+        calendarView.visibleDates { visibleDates in
+            self.setupViewsOfCalendar(from: visibleDates)
         }
     }
+    func handleCellTextColor(view: JTAppleCell?, cellState: CellState) {
+        guard let validCell = view as? DateCell  else { return }
+        if cellState.isSelected {
+            validCell.dateLabel.textColor = selectedMonthColor
+            
+        } else {
+            if cellState.dateBelongsTo == .thisMonth {
+                validCell.dateLabel.textColor = monthColor
+            } else {
+                validCell.dateLabel.textColor = outsideMonthColor
+            }
+        }
+    }
+    
+    func handleCellTextSelected(view: JTAppleCell?, cellState: CellState) {
+        guard let validCell = view as? DateCell  else { return }
+        if cellState.isSelected {
+            validCell.selectedView.isHidden = false
+        } else {
+            validCell.selectedView.isHidden = true
+        }
+    }
+    
+    func setupViewsOfCalendar(from visibleDates: DateSegmentInfo) {
+        
+        let date = visibleDates.monthDates.first!.date
+        let dateFormatter = DateFormatter()
+
+        dateFormatter.dateFormat = "YYYY"
+
+        year.text = dateFormatter.string(from: date)
+
+        dateFormatter.dateFormat = "MMMM"
+
+        month.text = dateFormatter.string(from: date)
+    }
+    
+    
 }
 
-extension CalendarViewController: JTAppleCalendarViewDataSource {
+extension CalendarViewController: JTAppleCalendarViewDataSource { 
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy MM dd"
-
+        
         let startDate = formatter.date(from: "2021 01 01")!
         let endDate = Date()
         return ConfigurationParameters(startDate: startDate, endDate: endDate)
@@ -51,33 +91,22 @@ extension CalendarViewController: JTAppleCalendarViewDataSource {
 extension CalendarViewController: JTAppleCalendarViewDelegate {
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as! DateCell
-        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
+        cell.dateLabel.text = cellState.text
+        handleCellTextSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
         return cell
     }
     
     func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
-        configureCell(view: cell, cellState: cellState)
-    }
-    
-    
-    func calendar(_ calendar: JTAppleCalendarView, headerViewForDateRange range: (start: Date, end: Date), at indexPath: IndexPath) -> JTAppleCollectionReusableView {
-        let formatter = DateFormatter()  // Declare this outside, to avoid instancing this heavy class multiple times.
-        formatter.dateFormat = "MMM"
         
-        let header = calendar.dequeueReusableJTAppleSupplementaryView(withReuseIdentifier: "DateHeader", for: indexPath) as! DateHeader
-        header.monthTitle.text = formatter.string(from: range.start)
-        return header
-    }
-
-    func calendarSizeForMonths(_ calendar: JTAppleCalendarView?) -> MonthSize? {
-        return MonthSize(defaultSize: 60)
     }
     
     
     func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellTextSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy"
-//        dateFormatter.locale=NSLocale(localeIdentifier: "en_US") as Locale
         let georgianDate = date
         
         let islamic = NSCalendar(identifier: NSCalendar.Identifier.islamic)
@@ -85,7 +114,27 @@ extension CalendarViewController: JTAppleCalendarViewDelegate {
         let dateString = String(format: "%02d-%02d-%04d", components.day!, components.month!, components.year!)
         islamicDate.text = dateString
         
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
+        setupViewsOfCalendar(from: visibleDates)
         
-        
+    }
+    
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellTextSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+}
+
+extension UIColor {
+    convenience init(colorWithHexValue value: Int, alpha:CGFloat = 1.0) {
+        self.init(
+            red: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((value & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(value & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
     }
 }
